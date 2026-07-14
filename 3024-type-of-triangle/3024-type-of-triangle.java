2ee226934f21848a37f5d128f280166ee2cd8da7
SANJAI MOHAN <sanjaimohan25@gmail.com>
2026-07-14 10:34:38 +0530

    Time: 3 ms (12.74%), Space: 44 MB (9.16%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String triangleType(int[] nums) {
+        Arrays.sort(nums);
+       HashSet set=new HashSet<>();
+       for(int n:nums){
+        set.add(n);
+       }
+       if(nums[0]+nums[1]<=nums[2]){
+        return "none";
+       }
+       if(set.size()==1){
+        return "equilateral";}
+        if(set.size()==2){
+            return "isosceles";
+        }
+        return "scalene";
+        
+    }
+}
