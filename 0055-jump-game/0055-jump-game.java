3617959f82c39b788e38643b635db478b1b8c4f9
SANJAI MOHAN <sanjaimohan25@gmail.com>
2026-08-03 10:06:11 +0530

    Time: 2 ms (88.59%), Space: 48.1 MB (23.89%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>ans){
+                return false;
+            }else{
+                ans=Math.max(ans,i+nums[i]);
+            }
+        }
+        return true;
+    }
+}
